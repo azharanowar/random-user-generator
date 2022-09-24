6594e7e828737abb2cad037d505afc158adc29b1
azharanowar <azharanowar@gmail.com>
2022-09-25 00:43:42 +0600

    Copy to clickboard function added for copy name by clicking

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 const getNewUserInfo = () => {
     fetch('https://randomuser.me/api')
-      .then(response => response.json())
-      .then(data => displayUserInfo(data))
+        .then(response => response.json())
+        .then(data => displayUserInfo(data))
 }
 
 
 const displayUserInfo = (data) => {
     console.log(data.results[0]);
     const userInfo = data.results[0];
-    
+
     document.getElementById("displayUserPhoto").src = userInfo.picture.large;
 
     const userName = `${userInfo.name.first} ${userInfo.name.last}`;
@@ -27,12 +27,12 @@ const displayUserInfo = (data) => {
     document.getElementById("displayUserGander").innerText = userGender;
 
     let userDOB = new Date(userInfo.dob.date);
-    userDOB = `${userDOB.getDate()}/${userDOB.getMonth() + 1}/${userDOB.getFullYear()}`
+    userDOB = `${userDOB.getDate()}/${userDOB.getMonth() + 1}/${userDOB.getFullYear()} (${userInfo.dob.age} years old)`
     document.getElementById("displayUserDOB").innerText = userDOB;
 
     const userAddress = `Street: ${userInfo.location.street.number} ${userInfo.location.street.name}, City: ${userInfo.location.city}, State: ${userInfo.location.state}, ${userInfo.location.country}.`;
     document.getElementById("displayUserAddress").innerText = userAddress;
-    
+
     const userPostCode = userInfo.location.postcode;
     document.getElementById("displayUserPostcode").innerText = userPostCode;
 
@@ -42,3 +42,31 @@ const displayUserInfo = (data) => {
 
 
 getNewUserInfo();
+
+
+document.getElementById("copyUserFullName").addEventListener('click', () => {
+    copyUserInfoToClipboard("displayUserFullName");
+    toastTriggered("copyUserFullName");
+});
+
+
+const copyUserInfoToClipboard = id => {
+    var copyText = document.getElementById(id).innerText;
+    navigator.clipboard.writeText(copyText).then(() => {
+    });
+}
+
+const toastTriggered = id => {
+    const toastTrigger = document.getElementById(id)
+    console.log(toastTrigger)
+    const toastLiveExample = document.getElementById('liveToast')
+    if (toastTrigger) {
+        toastTrigger.addEventListener('click', () => {
+            const toast = new bootstrap.Toast(toastLiveExample)
+
+            toast.show()
+        })
+    }
+}
+
+toastTriggered('');
