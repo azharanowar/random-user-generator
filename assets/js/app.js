8b097a70ece8df0cc76fa6a635b3122ee90a58e4
azharanowar <azharanowar@gmail.com>
2022-09-25 00:11:26 +0600

    Get user info and display the user information.

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+const getNewUserInfo = () => {
+    fetch('https://randomuser.me/api')
+      .then(response => response.json())
+      .then(data => displayUserInfo(data))
+}
+
+
+const displayUserInfo = (data) => {
+    console.log(data.results[0]);
+    const userInfo = data.results[0];
+    
+    document.getElementById("displayUserPhoto").src = userInfo.picture.large;
+
+    const userName = `${userInfo.name.first} ${userInfo.name.last}`;
+    document.getElementById("displayUserName").innerText = userName;
+
+    const userFullName = `${userInfo.name.title}. ${userInfo.name.first} ${userInfo.name.last}`;
+    document.getElementById("displayUserFullName").innerText = userFullName;
+
+    const userEmailAddress = userInfo.email;
+    document.getElementById("displayUserEmail").innerText = userEmailAddress;
+
+    const userPhoneNumber = userInfo.phone;
+    document.getElementById("displayUserPhoneNumber").innerText = userPhoneNumber;
+
+    const userGender = userInfo.gender;
+    document.getElementById("displayUserGander").innerText = userGender;
+
+    let userDOB = new Date(userInfo.dob.date);
+    userDOB = `${userDOB.getDate()}/${userDOB.getMonth() + 1}/${userDOB.getFullYear()}`
+    document.getElementById("displayUserDOB").innerText = userDOB;
+
+    const userAddress = `Street: ${userInfo.location.street.number} ${userInfo.location.street.name}, City: ${userInfo.location.city}, State: ${userInfo.location.state}, ${userInfo.location.country}.`;
+    document.getElementById("displayUserAddress").innerText = userAddress;
+    
+    const userPostCode = userInfo.location.postcode;
+    document.getElementById("displayUserPostcode").innerText = userPostCode;
+
+    const userSSNNumber = userInfo.id.value;
+    document.getElementById("displayUserSSNNumber").innerText = userSSNNumber;
+}
+
+
+getNewUserInfo();
